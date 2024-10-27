6c9d49e47285483f0bdeea60eb175b30c5054065
basnipes <basnipes@crimson.ua.edu>
2024-10-27 17:46:46 -0500

    Setup

---
+++ Fall2024-Assignment3-basnipes/wwwroot/js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert('This site offers AI-powered reviews and insights into your favorite movies and actors.');
+});
